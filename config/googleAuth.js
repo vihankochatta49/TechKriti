a29382493b4491ee65ca405b5ba3285d33de1fc0
Vihan <vihankochatta49@gmail.com>
2022-07-11 15:18:49 +0530

    CA Portal

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -8,7 +8,7 @@ passport.use(
       clientID:
         "195118986970-o3vldjfl0omasjphi6ue1ibpk5tfo29a.apps.googleusercontent.com",
       clientSecret: "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",
-      callbackURL: "http://localhost:3000/google/callback",
+      callbackURL: "https://ca-portal-techkriti-iitk.herokuapp.com/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
